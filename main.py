dce04c3e5c3501489ebb7a90eb564dbf4855c8f2
RAJA BABU KUMAR <96349517+kingrajakumar@users.noreply.github.com>
2022-01-24 17:52:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ from core.decorators import register, language, handle_error, only_admins
 
 REPO = """
  **Music Player**
-- Repo: [GitHub](https://github.com/LEGEND-OS/MUSIC-VC)
-- Owner: @The_LegendBoy
+- Repo: [channel](https://t.me/ABOUT_ABHINAS)
+- Owner: @abhinasroy
 """
 
 
